c566a1bc241f4833f00016c7f8bc8aa7fc715a0b
Piotrek <neopack1@gmail.com>
2015-12-31 10:26:27 +0100

    update

--- download.bat
+++ download.bat
@@ -58,9 +58,10 @@ echo.
 
 :CLEAR_TEMP
 pushd "%~dp0"
-TIMEOUT /T 3 /NOBREAK
+TIMEOUT /T 5 /NOBREAK
 
-"C:\Program Files\Git\git-bash.exe" "-q --no-cd script.sh %*"
+
+"C:\Program Files\Git\git-bash.exe" --no-cd "script.sh" %*
 TIMEOUT /T 60 /NOBREAK
 cls
 goto run
